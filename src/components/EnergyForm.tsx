a8d65c48ce0d0b48ec79b17c9106c55793da734f
Daniel Deak <daniel.deak@dataswift.io>
2021-05-16 09:15:45 +0100

    Re-designed the filter layout

--- src/components/EnergyForm.tsx
+++ src/components/EnergyForm.tsx
@@ -1,6 +1,17 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { TextField, Select, MenuItem, FormControl, InputLabel, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  makeStyles,
+  Paper,
+  Collapse,
+  Button,
+  Typography,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -8,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   },
   formRow: {
     display: 'grid',
-    gridTemplateColumns: '1fr 1fr 1fr'
+    gridTemplateColumns: '1fr 1fr 1fr',
   },
   formControl: {
     margin: theme.spacing(2),
@@ -21,8 +32,20 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: 'none',
     borderRadius: '6px',
     border: 'none',
-    margin: theme.spacing(2)
-  }
+    margin: theme.spacing(2),
+  },
+  filter: {
+    maxWidth: '50rem',
+    padding: '1rem',
+    minWidth: '700px',
+  },
+  filterButton: {
+    margin: '1rem',
+  },
+  section: {
+    padding: '2rem',
+    marginBottom: '1rem',
+  },
 }));
 
 type FormValues = {
@@ -41,9 +64,10 @@ type FormValues = {
 };
 
 const EnergyForm: React.FC = () => {
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
 
-  const {register, handleSubmit} = useForm<FormValues>({
+  const { register, handleSubmit } = useForm<FormValues>({
     defaultValues: {
       energyTypes: '',
       heatingTypes: '',
@@ -57,116 +81,150 @@ const EnergyForm: React.FC = () => {
       temperatureSummer: '',
       floorsNumber: '',
       energyUsage: '',
-    }
+    },
   });
 
   const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
-    <form onSubmit={onSubmit} className={classes.form}>
-      <div className={classes.formRow}>
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-controlled-open-select-label">Energy Type</InputLabel>
-          <Select {...register("energyTypes")}>
-            <MenuItem value={'grid'}>Grid</MenuItem>
-            <MenuItem value={'solar'}>Solar</MenuItem>
-          </Select>
-        </FormControl>
+    <div>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => setOpen(!open)}
+        className={classes.filterButton}
+      >
+        Toggle filter
+      </Button>
+      <form onSubmit={onSubmit} className={classes.form}>
+        <Collapse in={open}>
+          <Paper className={classes.filter}>
+            <Paper elevation={3} className={classes.section}>
+              <Typography variant="h5">Types</Typography>
+              <div className={classes.formRow}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Energy Type
+                  </InputLabel>
+                  <Select {...register('energyTypes')} defaultValue="">
+                    <MenuItem value={'grid'}>Grid</MenuItem>
+                    <MenuItem value={'solar'}>Solar</MenuItem>
+                  </Select>
+                </FormControl>
 
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-controlled-open-select-label">Heating Type</InputLabel>
-          <Select {...register("heatingTypes")}>
-            <MenuItem value={'heating pump'}>Heating Pump</MenuItem>
-            <MenuItem value={'natural gas'}>Natural Gas</MenuItem>
-            <MenuItem value={'absorption'}>Absorption</MenuItem>
-            <MenuItem value={'electric'}>Electric</MenuItem>
-          </Select>
-        </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Heating Type
+                  </InputLabel>
+                  <Select {...register('heatingTypes')} defaultValue="">
+                    <MenuItem value={'heating pump'}>Heating Pump</MenuItem>
+                    <MenuItem value={'natural gas'}>Natural Gas</MenuItem>
+                    <MenuItem value={'absorption'}>Absorption</MenuItem>
+                    <MenuItem value={'electric'}>Electric</MenuItem>
+                  </Select>
+                </FormControl>
 
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-controlled-open-select-label">Glazing Type</InputLabel>
-          <Select {...register("glazingTypes")}>
-            <MenuItem value={'single'}>Single</MenuItem>
-            <MenuItem value={'double'}>Double</MenuItem>
-            <MenuItem value={'triple'}>Triple</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Glazing Type
+                  </InputLabel>
+                  <Select {...register('glazingTypes')} defaultValue="">
+                    <MenuItem value={'single'}>Single</MenuItem>
+                    <MenuItem value={'double'}>Double</MenuItem>
+                    <MenuItem value={'triple'}>Triple</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
 
+              <div className={classes.formRow}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Vehicle Type
+                  </InputLabel>
+                  <Select {...register('vehicleTypes')} defaultValue="">
+                    <MenuItem value={'electric'}>Electric</MenuItem>
+                    <MenuItem value={'general'}>General</MenuItem>
+                  </Select>
+                </FormControl>
 
-      <div className={classes.formRow}>
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-controlled-open-select-label">Vehicle Type</InputLabel>
-          <Select {...register("vehicleTypes")}>
-            <MenuItem value={'electric'}>Electric</MenuItem>
-            <MenuItem value={'general'}>General</MenuItem>
-          </Select>
-        </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Domicile Type
+                  </InputLabel>
+                  <Select {...register('domicileTypes')} defaultValue="">
+                    <MenuItem value={'detached'}>Detached</MenuItem>
+                    <MenuItem value={'semi detached'}>Semi detached</MenuItem>
+                    <MenuItem value={'terraced'}>Terraced</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+            </Paper>
 
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-controlled-open-select-label">Domicile Type</InputLabel>
-          <Select {...register("domicileTypes")}>
-            <MenuItem value={'detached'}>Detached</MenuItem>
-            <MenuItem value={'semi detached'}>Semi detached</MenuItem>
-            <MenuItem value={'terraced'}>Terraced</MenuItem>
-          </Select>
-        </FormControl>
+            <Paper elevation={3} className={classes.section}>
+              <Typography variant="h5">Usage</Typography>
+              <div className={classes.formRow}>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    {...register('occupants')}
+                    label="Occupants"
+                    helperText="Example: 1 - 100"
+                  />
+                </FormControl>
 
-        <FormControl className={classes.formControl}>
-          <TextField
-            {...register("occupants")}
-            label="Occupants"
-            helperText="Example: 1 - 100"
-          />
-        </FormControl>
-      </div>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    {...register('yearsBuilt')}
+                    label="Years built"
+                    helperText="Example: 1 - 2021"
+                  />
+                </FormControl>
 
-      <div className={classes.formRow}>
-        <FormControl className={classes.formControl}>
-          <TextField
-            {...register("yearsBuilt")}
-            label="Years built"
-            helperText="Example: 1 - 2021"
-          />
-        </FormControl>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    {...register('numAppliances')}
+                    label="Number of Appliances"
+                    helperText="Example: 1 - 100"
+                  />
+                </FormControl>
+              </div>
+            </Paper>
 
-        <FormControl className={classes.formControl}>
-          <TextField
-            {...register("numAppliances")}
-            label="Number of Appliances"
-            helperText="Example: 1 - 100"
-          />
-        </FormControl>
+            <Paper elevation={3} className={classes.section}>
+              <Typography variant="h5">Energy</Typography>
+              <div className={classes.formRow}>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    {...register('temperatureWinter')}
+                    label="Winter Temperate"
+                    helperText="Example: -100 - 100"
+                  />
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    {...register('temperatureSummer')}
+                    label="Summer Temperature"
+                    helperText="Example: 1 - 100"
+                  />
+                </FormControl>
 
-        <FormControl className={classes.formControl}>
-          <TextField
-            {...register("temperatureWinter")}
-            label="Winter Temperate"
-            helperText="Example: -100 - 100"
-          />
-        </FormControl>
-      </div>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    {...register('energyUsage')}
+                    label="Energy Usage"
+                    helperText="Example: 1 - 100000"
+                  />
+                </FormControl>
+              </div>
+            </Paper>
 
-      <div className={classes.formRow}>
-        <FormControl className={classes.formControl}>
-          <TextField
-            {...register("temperatureSummer")}
-            label="Summer Temperature"
-            helperText="Example: 1 - 100"
-          />
-        </FormControl>
-
-        <FormControl className={classes.formControl}>
-          <TextField
-            {...register("energyUsage")}
-            label="Energy Usage"
-            helperText="Example: 1 - 100000"
-          />
-        </FormControl>
-      </div>
-      <button type="submit" className={classes.submitButton}>Submit</button>
-    </form>
-  )
-}
+            <button type="submit" className={classes.submitButton}>
+              Submit
+            </button>
+          </Paper>
+        </Collapse>
+      </form>
+    </div>
+  );
+};
 
 export default EnergyForm;
