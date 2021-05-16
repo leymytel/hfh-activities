eb1fcbed122ec9b517dc07155b2ad28021c5f269
Ty <tyler.weir@dataswift.io>
2021-05-16 13:12:35 -0400

    WIP

--- src/testData/Activities.ts
+++ src/testData/Activities.ts
@@ -3,26 +3,66 @@ import { ActivityInterface } from "../interfaces/ActivityInterface";
 export const TEST_DATA_ACTIVITIES: ActivityInterface[] = [
   {
     id: 'unique-id-1',
-    title: 'Activity Test 1',
-    ranking: 10,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    title: '<font color="green">numE Reccommended! (78%)</font> Add solar panels',
+    ranking: 100,
+    description: 'Solar Panels are a great way to reduce your energy load on the grid, you may even be able to resell your unused energy!',
     dateCreated: '2021-05-15T10:32:47.642Z',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor orci ut, semper est. Aliquam erat volutpat. Aliquam eget turpis vitae massa condimentum tempus. Phasellus massa libero, accumsan euismod quam id, porta rutrum neque. Nunc dapibus felis ut felis suscipit, nec sollicitudin lorem luctus. Nullam sollicitudin iaculis ligula in ultricies. Donec dictum ante et libero ornare elementum. Nulla facilisi. Quisque sit amet ex mattis, porttitor ex quis, facilisis orci. Aenean venenatis quam condimentum commodo fermentum. Phasellus pretium nulla consectetur quam gravida, eu varius tellus varius.'
+    details: ''
   },
   {
     id: 'unique-id-2',
-    title: 'Activity Test 2',
-    ranking: 20,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    title: 'Replace your heating system with a Heat Pump',
+    ranking: 90,
+    description: 'Heat Pumps efficiently cool and heat areas based on energy transfer.',
     dateCreated: '2021-05-14T12:32:47.642Z',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor orci ut, semper est. Aliquam erat volutpat. Aliquam eget turpis vitae massa condimentum tempus. Phasellus massa libero, accumsan euismod quam id, porta rutrum neque. Nunc dapibus felis ut felis suscipit, nec sollicitudin lorem luctus. Nullam sollicitudin iaculis ligula in ultricies. Donec dictum ante et libero ornare elementum. Nulla facilisi. Quisque sit amet ex mattis, porttitor ex quis, facilisis orci. Aenean venenatis quam condimentum commodo fermentum. Phasellus pretium nulla consectetur quam gravida, eu varius tellus varius.'
+    details: ''
   },
   {
     id: 'unique-id-3',
-    title: 'Activity Test 3',
-    ranking: 30,
+    title: 'Increase your Insulation R-Value',
+    ranking: 80,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
     dateCreated: '2021-05-13T09:32:47.642Z',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor orci ut, semper est. Aliquam erat volutpat. Aliquam eget turpis vitae massa condimentum tempus. Phasellus massa libero, accumsan euismod quam id, porta rutrum neque. Nunc dapibus felis ut felis suscipit, nec sollicitudin lorem luctus. Nullam sollicitudin iaculis ligula in ultricies. Donec dictum ante et libero ornare elementum. Nulla facilisi. Quisque sit amet ex mattis, porttitor ex quis, facilisis orci. Aenean venenatis quam condimentum commodo fermentum. Phasellus pretium nulla consectetur quam gravida, eu varius tellus varius.'
+    details: ''
+  },
+  {
+    id: 'unique-id-4',
+    title: 'Replace your Single Glazed Windows, with Double Glazing',
+    ranking: 70,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    dateCreated: '2021-05-15T10:32:47.642Z',
+    details: ''
+  },
+  {
+    id: 'unique-id-5',
+    title: 'Reduce your count of Applicances',
+    ranking: 60,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    dateCreated: '2021-05-14T12:32:47.642Z',
+    details: ''
+  },
+  {
+    id: 'unique-id-6',
+    title: 'Replace Aging Appliances with high efficiency ones',
+    ranking: 50,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    dateCreated: '2021-05-13T09:32:47.642Z',
+    details: ''
+  },
+  {
+    id: 'unique-id-7',
+    title: 'Reduce your average desired winter temperature.',
+    ranking: 40,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    dateCreated: '2021-05-15T10:32:47.642Z',
+    details: ''
+  },
+  {
+    id: 'unique-id-8',
+    title: 'Increase you average desired summer temperature.',
+    ranking: 30,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum justo dui. Donec mattis enim vehicula, tempor',
+    dateCreated: '2021-05-14T12:32:47.642Z',
+    details: ''
   }
 ]
